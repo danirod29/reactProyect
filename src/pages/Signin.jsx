80bd82c4bb2c93b943fd394713a114fdd7d6a368
danirod29 <51457784+danirod29@users.noreply.github.com>
2023-11-10 18:56:52 -0500

    primer commit

---
+++ src/pages/Signin.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Landing from "../components/signig/Landing";
+
+const Signin = () =>{
+    return (
+        <div data-theme="light">
+            <Landing />
+        </div>
+    );
+}
+
+export default Signin
